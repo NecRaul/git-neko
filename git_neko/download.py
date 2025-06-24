095f0f944c724e20635c91ce58299616886de9d1
NecRaul <necraul@kuroneko.dev>
2025-06-24 04:22:03 +0400

    Improve code quality
    Only return repos belonging to the user from get_all_repos.
    Instead of looping through all repos and passing each repo
    to a function based on check, pass all of repos to the
    appropriate function and loop over them inside the function.

--- git_neko/download.py
+++ git_neko/download.py
@@ -5,33 +5,35 @@ import shutil
 import tarfile
 
 
-def with_request(repo, headers):
-    repo_name = repo["name"]
-    tarball_url = f"{repo['html_url']}/tarball/{repo['default_branch']}"
-    if not os.path.exists(repo_name):
-        os.mkdir(repo_name)
-        print(f"Downloading the repository '{repo_name}'...")
-    else:
-        print(f"Updating the repository '{repo_name}'...")
-        shutil.rmtree(repo_name)
-    response = requests.get(tarball_url, headers=headers)
-    with open(f"{repo_name}.tar.gz", "wb") as file:
-        file.write(response.content)
-    with tarfile.open(f"{repo_name}.tar.gz", "r:gz") as tar_ref:
-        tar_info = tar_ref.getmembers()[1:]
-        for member in tar_info:
-            member.name = f"{repo_name}/{member.name.split('/', 1)[-1]}"
-            tar_ref.extract(member)
-    os.remove(f"{repo_name}.tar.gz")
+def download_with_requests(repos, headers):
+    for repo in repos:
+        repo_name = repo["name"]
+        tarball_url = f"{repo['html_url']}/tarball/{repo['default_branch']}"
+        if not os.path.exists(repo_name):
+            os.mkdir(repo_name)
+            print(f"Downloading the repository '{repo_name}'...")
+        else:
+            print(f"Updating the repository '{repo_name}'...")
+            shutil.rmtree(repo_name)
+        response = requests.get(tarball_url, headers=headers)
+        with open(f"{repo_name}.tar.gz", "wb") as file:
+            file.write(response.content)
+        with tarfile.open(f"{repo_name}.tar.gz", "r:gz") as tar_ref:
+            tar_info = tar_ref.getmembers()[1:]
+            for member in tar_info:
+                member.name = f"{repo_name}/{member.name.split('/', 1)[-1]}"
+                tar_ref.extract(member)
+        os.remove(f"{repo_name}.tar.gz")
 
 
-def with_git(repo):
-    repo_name = repo["name"]
-    repo_pull_url = repo["ssh_url"]
-    if not os.path.exists(repo_name):
-        subprocess.call(["git", "clone", "--recursive", repo_pull_url])
-    else:
-        subprocess.call(["git", "-C", repo_name, "pull", "--recurse-submodules"])
+def download_with_git(repos):
+    for repo in repos:
+        repo_name = repo["name"]
+        repo_pull_url = repo["ssh_url"]
+        if not os.path.exists(repo_name):
+            subprocess.call(["git", "clone", "--recursive", repo_pull_url])
+        else:
+            subprocess.call(["git", "-C", repo_name, "pull", "--recurse-submodules"])
 
 
 def get_all_repos(username, token, headers):
@@ -59,14 +61,14 @@ def get_all_repos(username, token, headers):
         repos.extend(page_repos)
         page += 1
 
-    return repos
+    return [repo for repo in repos if username in repo["full_name"]]
 
 
 def download_repositories(username, token, git_check):
     headers = {"Authorization": f"token {token}"} if token else None
     repos = get_all_repos(username, token, headers)
 
-    for repo in repos:
-        if not username in repo["full_name"]:
-            continue
-        with_request(repo, headers) if not git_check else with_git(repo)
+    if git_check:
+        download_with_git(repos)
+    else:
+        download_with_requests(repos, headers)
